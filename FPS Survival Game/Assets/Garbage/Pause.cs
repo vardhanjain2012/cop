5eb6133f521850358edfabd833c86a93b76e347a
Vardhan Jain <vardhanjain2012@gmail.com>
2019-05-07 00:33:10 +0530

    phase1

---
+++ FPS Survival Game/Assets/Garbage/Pause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool IsPaused=false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        } 
+    }
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+            Cursor.visible = false;
+        }
+        void Paused()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            IsPaused = true;
+        }
+    public void LoadMenu()
+    {
+        Debug.Log("hhk");
+        SceneManager.LoadScene(0);
+    }
+    public void QuitMenu()
+    {
+        Debug.Log("hh");
+        Application.Quit();
+    }
+}
---
+++ FPS Survival Game/Assets/Garbage/exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour
+{
+    public void Restartgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Tomenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Quitgame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
