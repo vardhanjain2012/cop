f091d83faa2ac6ecdadaf4c3eac4b97b059035d9
vardhanjain2012 <45758099+vardhanjain2012@users.noreply.github.com>
2019-05-07 13:19:09 +0530

    phase3
    game is completed

---
+++ Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+//using TMPro;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController character_Controller;
+    private PlayerStats p1;
+    GameObject pp;
+    private Vector3 move_Direction;
+
+    public float fruit_gain=7f;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump_Force = 10f;
+    private float vertical_Velocity;
+    void Start()
+    {
+        pp = GameObject.FindWithTag("Player");
+        p1 = pp.GetComponent<PlayerStats>();
+        if (p1 == null)
+        {
+            Debug.Log("dfdd");
+        }
+    }
+    void Awake() {
+        character_Controller = GetComponent<CharacterController>();
+    }
+	
+	void Update () {
+        MoveThePlayer();
+	}
+
+    void MoveThePlayer() {
+
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f,
+                                     Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        character_Controller.Move(move_Direction);
+
+
+    } // move player
+
+    void ApplyGravity() {
+
+        vertical_Velocity -= gravity * Time.deltaTime;
+
+        // jump
+        PlayerJump();
+
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    } // apply gravity
+
+    void PlayerJump() {
+
+        if(character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            vertical_Velocity = jump_Force;
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("fruit"))
+        {
+
+            other.gameObject.SetActive(false);
+            if (p1.health_Stats.fillAmount + (fruit_gain/100f) > 1f)
+            {
+                p1.health_Stats.fillAmount = 1f;
+            }
+            else
+            {
+                p1.health_Stats.fillAmount = p1.health_Stats.fillAmount + (fruit_gain / 100f);
+            }
+            // count = count + 1;
+            //CountUpdate();
+        }
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Scripts/Player Scripts/PlayerStats.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+    private PlayerStats p1;
+    GameObject pp;
+
+
+//[SerializeField]
+    public Image health_Stats, stamina_Stats,environment_stats;
+    public float min_environment=0.3f;
+    //public Text wintext;
+    public Text scoretext;
+    public Text timetext;
+    private int dtime;
+    private int ctime;
+    private int score = 0;
+    void Start()
+    {
+        //wintext.text = "";
+        score = 0;
+        dtime = 300-(int)Time.time;
+        ctime = 300;
+        TimeUpdate();
+        ScoreUpdate();
+        //WinUpdate();
+        pp= GameObject.FindWithTag("Player");
+        p1 = pp.GetComponent<PlayerStats>();
+        if (p1 == null)
+        {
+            Debug.Log("dfdd");
+        }
+        Display_HealthStats(70f);
+    }
+    void ScoreUpdate()
+    {
+        scoretext.text = "SCORE: " + score.ToString();
+    }
+    void TimeUpdate()
+    {
+        timetext.text = "TimeLeft: " + dtime.ToString();
+    }
+    void Update()
+    {
+        score = (int)(100f * 100f * (0.1f+health_Stats.fillAmount)*(environment_stats.fillAmount));
+        ScoreUpdate();
+        dtime = 300-(int)Time.time;
+        TimeUpdate();
+        if (ctime - dtime>=1)
+        {
+            health_Stats.fillAmount = health_Stats.fillAmount - (((ctime - dtime) * 0.8f) / 150);
+            ctime = dtime;
+        }
+        if (dtime <= 0)
+        {
+            Invoke("QuitMenu", 3f);
+        }
+        if (p1.environment_stats.fillAmount < min_environment)
+        {
+            Invoke("QuitMenu", 3f);
+        }
+    }
+    public void Display_HealthStats(float healthValue) {
+
+        healthValue /= 100f;
+
+        health_Stats.fillAmount = healthValue;
+
+    }
+
+    public void Display_StaminaStats(float staminaValue) {
+
+        staminaValue /= 100f;
+        stamina_Stats.fillAmount =  staminaValue;
+
+    }
+    public void Display_EnvironmentStats(float EnvironmentValue)
+    {
+
+        EnvironmentValue /= 100f;
+
+        environment_stats.fillAmount = environment_stats.fillAmount - EnvironmentValue;
+
+    }
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
